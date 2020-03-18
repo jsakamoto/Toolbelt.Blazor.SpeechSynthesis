89ffeb70ae8fc884c2b40398b7947cad794666f0
jsakamoto <junichi.jp@gmail.com>
2020-03-18 14:20:08 +0900

    Update sample site to Blazor v.3.2 preview 2.

--- SampleSites/SampleSite.Client/Program.cs
+++ SampleSites/SampleSite.Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using SampleSite.Components;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
@@ -11,6 +12,7 @@ namespace SampleSite.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+            builder.Services.AddBaseAddressHttpClient();
             builder.Services.AddSpeechSynthesis(options =>
             {
                 // options.DisableClientScriptAutoInjection = true;
